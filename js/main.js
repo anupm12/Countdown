844dd6bec5a5416d611f68af7c6bc3a0e67fc851
anupm12 <shuklaanupam18@gmail.com>
2020-11-27 00:42:57 +0530

    Frontend

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var targetDate = new Date("27 nov 2020").getTime();
+var targetDate = new Date("27 dec 2020").getTime();
 
 function counter() {
   var dateToday = new Date().getTime();
@@ -11,9 +11,14 @@ function counter() {
 
   var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
 
-  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  var seconds = Math.floor(1 + (diff % (1000 * 60)) / 1000);
 
-  console.log(days, hours, minutes, seconds);
+  // console.log(days, hours, minutes, seconds);
+
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
 }
 counter();
 setInterval(counter, 1000);
