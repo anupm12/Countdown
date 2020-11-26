e1d2a2d12fa3271a05ffa45bebba9c60173e9e81
anupm12 <shuklaanupam18@gmail.com>
2020-11-27 01:04:52 +0530

    Add 0 to single digits

--- js/main.js
+++ js/main.js
@@ -6,12 +6,16 @@ function counter() {
   var diff = targetDate - dateToday;
 
   var days = Math.floor(diff / (1000 * 24 * 60 * 60));
+  days = ("0" + days).slice(-2);
 
   var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  hours = ("0" + hours).slice(-2);
 
   var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  minutes = ("0" + minutes).slice(-2);
 
   var seconds = Math.floor(1 + (diff % (1000 * 60)) / 1000);
+  seconds = ("0" + seconds).slice(-2);
 
   // console.log(days, hours, minutes, seconds);
 
