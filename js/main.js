49819566f1f9227194f4def617d0842de8913fc4
anupm12 <shuklaanupam18@gmail.com>
2020-11-28 03:11:33 +0530

    Format date input

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-var targetDate = new Date("12 jan 2021").getTime();
+var targetDate = 0;
 
-localStorage.setItem("date", targetDate);
-console.log(localStorage.getItem("date"));
+// localStorage.setItem("date", targetDate);
+// console.log(localStorage.getItem("date"));
+
+function convertDate(date) {
+  let x = date;
+  let monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let yy = x.slice(0, 4);
+  let mmIndex = x.slice(5, 7);
+  let mm = monthNames[mmIndex - 1];
+  let dd = x.slice(8, 10);
+
+  return dd + " " + mm + " " + yy;
+}
+
+function formatTime(x) {
+  return x < 10 ? "0" + x : x;
+}
 
 function counter() {
   var dateToday = new Date().getTime();
@@ -9,14 +38,9 @@ function counter() {
   var diff = (targetDate - dateToday) / 1000;
 
   var days = Math.floor(diff / 3600 / 24);
-
   var hours = Math.floor(diff / 3600) % 24;
-
   var minutes = Math.floor(diff / 60) % 60;
-
   var seconds = Math.floor(1 + (diff % 60));
-
-  // console.log(days, hours, minutes, seconds);
 
   document.getElementById("day").innerHTML = formatTime(days);
   document.getElementById("hour").innerHTML = formatTime(hours);
@@ -24,9 +48,14 @@ function counter() {
   document.getElementById("second").innerHTML = formatTime(seconds);
 }
 
-function formatTime(x) {
-  return x < 10 ? "0" + x : x;
+function startHere() {
+  let inputDate = document.querySelector("#dateVal").value;
+  targetDate = new Date(convertDate(inputDate)).getTime();
+  console.log(targetDate);
+  counter();
+  setInterval(counter, 1000);
 }
 
-counter();
-setInterval(counter, 1000);
+document.getElementById("submitDate").onclick = function () {
+  startHere();
+};
